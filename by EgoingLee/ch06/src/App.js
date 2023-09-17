d3ad1923b1cdf7bd5dd7a808c87dd60c2df1c4b5
ekkk1126 <cabi0203@naver.com>
2023-09-17 18:12:57 +0900

    ch06; event

---
+++ by EgoingLee/ch06/src/App.js
@@ -0,0 +1,57 @@
+/** React ch05 : Props */
+function Header(props) {
+  return <header>
+    <h1><a href="/" onClick={(event) => {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(
+      <li key={t.id}>
+        <a id={t.id} href={"/read/" + t.id} onClick={(event) => {
+          event.preventDefault();
+          props.onChangeMode(event.target.id);
+        }}>{t.title}</a>
+      </li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "HTML", body: "HTML is ..." },
+    { id: 2, title: "CSS", body: "CSS is ..." },
+    { id: 3, title: "JavaScript", body: "JavaScript is ..." }
+  ]
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={() => {
+        alert("Header");
+      }}></Header>
+      <Nav topics={topics} onChangeMode={(id) => {
+        alert(id);
+      }}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
+
+    </div>
+  );
+}
+
+export default App;
