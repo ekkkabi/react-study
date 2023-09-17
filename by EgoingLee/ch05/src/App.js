9fc438dc56bbbb61e483f00c59551b21483b7d21
ekkk1126 <cabi0203@naver.com>
2023-09-17 17:42:48 +0900

    ch05; props

---
+++ by EgoingLee/ch05/src/App.js
@@ -0,0 +1,44 @@
+/** React ch05 : Props */
+function Header(props) {
+  return <header>
+    <h1><a href="/">{props.title}</a></h1>
+  </header>
+}
+
+function Nav(props) {
+  const lis = [];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={"/read/" + t.id}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
+function App() {
+  const topics = [
+    { id: 1, title: "HTML", body: "HTML is ..." },
+    { id: 2, title: "CSS", body: "CSS is ..." },
+    { id: 3, title: "JavaScript", body: "JavaScript is ..." }
+  ]
+  return (
+    <div>
+      <Header title="WEB"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello, WEB!"></Article>
+
+    </div>
+  );
+}
+
+export default App;
