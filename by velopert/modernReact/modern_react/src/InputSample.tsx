5d2fa9c2f2a91690c8842453d399a211335c7132
ekkk1126 <cabi0203@naver.com>
2024-01-26 01:33:18 +0900

    feat : input 관리 및 useRef

---
+++ by velopert/modernReact/modern_react/src/InputSample.tsx
@@ -0,0 +1,47 @@
+import React, { HtmlHTMLAttributes, useRef, useState } from "react";
+
+const InputSample = () => {
+  const [inputs, setInputs] = useState({
+    name: "",
+    nickname: "",
+  });
+  const nameInput = useRef<HTMLInputElement>();
+  const { name, nickname } = inputs;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+
+  const onReset = () => {
+    setInputs({
+      name: "",
+      nickname: "",
+    });
+    if (nameInput && nameInput.current) {
+      nameInput.current.focus();
+    }
+  };
+
+  return (
+    <div>
+      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="nickname"
+        placeholder="닉네임"
+        onChange={onChange}
+        value={nickname}
+      />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b>
+          {name}({nickname})
+        </b>
+      </div>
+    </div>
+  );
+};
+export default InputSample;
