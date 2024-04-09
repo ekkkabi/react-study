df6be219a9a694aa0d6ab081cb58d23f29d52530
ekkk1126 <cabi0203@naver.com>
2024-04-10 03:23:08 +0900

    [240410] S2 : Pagination, Prefetching and Mutation

--- react-query/base-blog-em/src/PostDetail.jsx
+++ react-query/base-blog-em/src/PostDetail.jsx
@@ -1,21 +1,66 @@
-import { fetchComments } from "./api";
-import "./PostDetail.css";
+import { useQuery } from '@tanstack/react-query';
+import { fetchComments } from './api';
+import './PostDetail.css';
 
-export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+export function PostDetail({ post, deleteMutation, updateMutation }) {
+    const { data, isError, isLoading } = useQuery({
+        queryKey: ['comments'],
+        queryFn: () => fetchComments(post.id),
+        staleTime: 2000,
+    });
 
-  return (
-    <>
-      <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
-      <p>{post.body}</p>
-      <h4>Comments</h4>
-      {data.map((comment) => (
-        <li key={comment.id}>
-          {comment.email}: {comment.body}
-        </li>
-      ))}
-    </>
-  );
+    if (isLoading) {
+        return <h3>Loading...</h3>;
+    }
+
+    if (isError) {
+        return <h3>Opps, something went wrong!</h3>;
+    }
+
+    return (
+        <>
+            <h3 style={{ color: 'blue' }}>{post.title}</h3>
+            <div>
+                <button onClick={() => deleteMutation.mutate(post.id)}>
+                    Delete
+                </button>
+                {deleteMutation.isPending && (
+                    <p className="loading">Deleting the post</p>
+                )}
+                {deleteMutation.isError && (
+                    <p className="error">
+                        Error deleting the post :
+                        {deleteMutation.error.toString()}
+                    </p>
+                )}
+                {deleteMutation.isSuccess && (
+                    <p className="success">Post was deleted. only client.</p>
+                )}
+            </div>
+            <div>
+                <button onClick={() => updateMutation.mutate(post.id)}>
+                    Update title
+                </button>
+                {updateMutation.isPending && (
+                    <p className="loading">Deleting the post</p>
+                )}
+                {updateMutation.isError && (
+                    <p className="error">
+                        Error deleting the post :
+                        {deleteMutation.error.toString()}
+                    </p>
+                )}
+                {updateMutation.isSuccess && (
+                    <p className="success">Post was updated. only client.</p>
+                )}
+            </div>
+            <p>{post.body}</p>
+            <h4>Comments</h4>
+            {data.map((comment) => (
+                <li key={comment.id}>
+                    {comment.email}: {comment.body}
+                </li>
+            ))}
+        </>
+    );
 }
