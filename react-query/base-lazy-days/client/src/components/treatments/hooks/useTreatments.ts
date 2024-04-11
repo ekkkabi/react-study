15a0068ae9125e86428fe2375c6275aaa4b0718a
ekkk1126 <cabi0203@naver.com>
2024-04-11 18:08:41 +0900

    [240411] S4 : 쿼리 설정, 집중, 커스텀 훅

---
+++ react-query/base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -0,0 +1,25 @@
+import type { Treatment } from '@shared/types';
+
+import { axiosInstance } from '@/axiosInstance';
+import { queryKeys } from '@/react-query/constants';
+import { useQuery } from '@tanstack/react-query';
+
+// for when we need a query function for useQuery
+// axios 인스턴스와 endpoint가 treatments 인 데이터를 가져옴
+async function getTreatments(): Promise<Treatment[]> {
+    const { data } = await axiosInstance.get('/treatments');
+    return data;
+}
+
+// 서버에서 treatement 데이터를 가져오기 위한 훅
+export function useTreatments(): Treatment[] {
+    // 서버에서 데이터를 가져오지 못할 경우
+    const fallback: Treatment[] = [];
+
+    // get data from server via useQuery
+    const { data = fallback } = useQuery({
+        queryKey: [queryKeys.treatments],
+        queryFn: getTreatments,
+    });
+    return data;
+}
